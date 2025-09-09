9dba3876da83e677d7c34fc9137f5c984ee94f5e
Nathan <148985551+nathan-004@users.noreply.github.com>
2025-09-09 12:43:14 +0200

    feat: affichage du nombre de FPS

--- simulation/rendering/rendering2D.py
+++ simulation/rendering/rendering2D.py
@@ -1,10 +1,13 @@
 import pygame
+import time
 
 from simulation.physics.particle import Particle
 from simulation.utils.constants import SCREEN_WIDTH, SCREEN_HEIGHT, DEFAULT_BACKGROUND_COLOR, DEFAULT_PARTICLE_COLOR, FPS
 
 pygame.init()
+pygame.font.init()
 
+fps_text = pygame.font.SysFont('Comic Sans MS', 30)
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 running = True
@@ -25,14 +28,22 @@ def render_particles(particles:list[Particle]) -> None:
 def main_game_loop():
     def decorator(func):
         def wrapper(*args, **kwargs):
+            last_time = time.time()
+            current_time = time.time()
+            current_fps = 0
             while running:
+                current_time = time.time()
+                current_fps = 60 // (current_time - last_time)
+                fps_text_surface = fps_text.render(f"FPS : {current_fps}", False, (0, 0, 0))
+                screen.blit(fps_text_surface, (0,0))
                 for event in pygame.event.get():
                     if event.type == pygame.QUIT:
                         return
-                    
+
                 func(*args, **kwargs)
 
                 pygame.display.flip()
+                last_time = time.time()
                 clock.tick(FPS)
             pygame.quit()
         return wrapper
